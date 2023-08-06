93ff4530b7a65f474651d8aa21f29bfb248985f4
Stefan <stefantaitano@gmail.com>
2023-08-05 18:18:46 -0600

    setup basic express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.status(200).json({message: 'Welcome to The Company Support Page API'})
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
