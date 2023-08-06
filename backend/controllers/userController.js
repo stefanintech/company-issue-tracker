67dbca87c83f6c0924f3ba066515aa66e3c0c61d
Stefan <stefantaitano@gmail.com>
2023-08-05 18:53:08 -0600

    add routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,13 @@
+// @desc   register a new user
+// @route  /api/users
+// @access public
 const registerUser = (req, res) => {
     res.send('Register Route')
 }
 
+// @desc   login a new user
+// @route  /api/users/login
+// @access public
 const loginUser = (req, res) => {
     res.send('Login Route')
 }
