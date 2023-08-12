d2523d10d26d9fa62be9df4eb876789fe32e305d
Stefan <stefantaitano@gmail.com>
2023-08-12 12:08:26 -0600

    connected to database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,26 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc   register a new user
 // @route  /api/users
 // @access public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body
+
+    // validation
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields')
+    }
+
     res.send('Register Route')
-}
+})
 
 // @desc   login a new user
 // @route  /api/users/login
 // @access public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.send('Login Route')
-}
+})
 
 module.exports = {
     registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 3000
 
+// connect to database
+connectDB()
+
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req,res) => {
     res.status(200).json({message: 'Welcome to The Company Support Page API'})
@@ -10,5 +19,7 @@ app.get('/', (req,res) => {
 
 // routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
