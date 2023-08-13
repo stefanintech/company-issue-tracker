dad8195509bad3c480a44e424bb1544d7769e232
Stefan <stefantaitano@gmail.com>
2023-08-13 11:12:20 -0600

    add protector route authentication for backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel')
 
@@ -38,24 +39,59 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user._id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
      } else {
         res.status(400)
-        throw new error('Invalid user data')
+        throw new Error('Invalid user data')
      }
-
-    res.send('Register Route')
 })
 
 // @desc   login a new user
 // @route  /api/users/login
 // @access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.send('Login Route')
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+    
+    // check user and password match
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid credentials')
+    }
 })
+
+// @desc   get current user
+// @route  /api/users/me
+// @access private
+const getMe = asyncHandler(async (req, res) => {
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name
+    }
+    res.status(200).json(user)
+
+})
+
+ // generate token
+ const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '14d'
+    })
+ }
 
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getMe
 }
