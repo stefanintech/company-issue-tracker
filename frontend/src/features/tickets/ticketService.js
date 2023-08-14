5fccefd5e8d2d973e47a1872f3f3ec9a1a7722e4
Stefan <stefantaitano@gmail.com>
2023-08-13 19:23:30 -0600

    update frontend to show tickets

---
+++ frontend/src/features/tickets/ticketService.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const API_URL = "/api/tickets/";
+
+// create new ticket
+const createTicket = async (ticketData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.post(API_URL, ticketData, config);
+
+  return response.data;
+};
+
+// get user ticket
+const getTickets = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL, config);
+
+  return response.data;
+};
+
+const ticketService = {
+  createTicket,
+  getTickets,
+};
+
+export default ticketService;
--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,19 +1,37 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {toast} from 'react-toastify'
+import {createTicket, reset} from '../features/tickets/ticketSlice'
+import BackButton from "../components/BackButton";
 
 function NewTicket() {
   const { user } = useSelector((state) => state.auth);
+  
+
+
   const [name] = useState(user.name);
   const [email] = useState(user.email);
   const [issue, setIssue] = useState("Voucher");
-  const [description, setDescription] = useState(user.description);
+  const [description, setDescription] = useState('');
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const onSubmit = (e) => {
-    e.preventDefault();
-  };
+    e.preventDefault()
+    dispatch(createTicket({ issue, description }))
+      .unwrap()
+      .then(() => {
+        navigate('/tickets')
+        toast.success('New ticket created!')
+      })
+      .catch(toast.error)
+  }
 
   return (
     <>
+    <BackButton url='/' />
       <section className="heading">
         <h1>Create New Ticket</h1>
         <p>Please fill out the form below</p>
