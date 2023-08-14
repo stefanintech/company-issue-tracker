0a2d2701b692671382847b0cdcde43f55f407620
Stefan <stefantaitano@gmail.com>
2023-08-13 18:29:47 -0600

    create new ticket form

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,13 @@
+import { FaArrowCircleLeft } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
+
+const BackButton = () => {
+  const navigate = useNavigate()
+  return (
+    <button className='btn btn-reverse btn-back' onClick={() => navigate(-1)}>
+      <FaArrowCircleLeft /> Back
+    </button>
+  )
+}
+
+export default BackButton
--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,7 +1,71 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
 function NewTicket() {
+  const { user } = useSelector((state) => state.auth);
+  const [name] = useState(user.name);
+  const [email] = useState(user.email);
+  const [issue, setIssue] = useState("Voucher");
+  const [description, setDescription] = useState(user.description);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <h1>New Ticket</h1>
-  )
+    <>
+      <section className="heading">
+        <h1>Create New Ticket</h1>
+        <p>Please fill out the form below</p>
+      </section>
+
+      <section className="form">
+        <div className="form-group">
+          <label htmlFor="name">Customer Name</label>
+          <input type="text" className="form-control" value={name} disabled />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Customer Email</label>
+          <input type="text" className="form-control" value={email} disabled />
+        </div>
+
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="issue">Issue</label>
+            <select
+              name="issue"
+              id="issue"
+              value={issue}
+              onChange={(e) => setIssue(e.target.value)}
+            >
+              <option value="Voucher">Voucher</option>
+              <option value="Government Travel Card">
+                Government Travel Card
+              </option>
+              <option value="Inprocessing">Inprocessing</option>
+              <option value="Outprocessing">Outprocessing</option>
+              <option value="Itinerary">Itinerary</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description of the issue</label>
+            <textarea
+              name="description"
+              id="description"
+              className="form-control"
+              placeholder="Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default NewTicket
+export default NewTicket;
